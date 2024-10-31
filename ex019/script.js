f141d0d09ff17016c49abde44e9923c1da753e03
Johnatan Oliveira Ferreira <johnatanoliveira353@gmail.com>
2024-10-31 19:44:32 -0300

    Add files via upload

--- ex019/script.js
+++ ex019/script.js
@@ -1,4 +1,5 @@
 var base = Number(prompt("digite a base do triangulo"))
 var altura = Number(prompt("digite a altura do triangulo"))
 var resultado = base * altura
-alert(`o resultado é ${resultado}`)
+var resultadoT = resultado / 2
+alert(`o resultado é ${resultadoT}`)
